76c771c7ebb637a0ed28671ef06154aa343b8fd1
AbdiLouay <ablouay5@gmail.com>
2024-09-05 11:26:41 +0200

    4

---
+++ script.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+    // Vérifier si un cookie de session existe
+    var token = getCookie('token');
+    if (token) {
+        // Si un cookie de session existe, afficher le contenu de la page après la connexion réussie
+        document.getElementById('login-form').style.display = 'none';
+        document.getElementById('login-success').style.display = 'block';
+    } else {
+        // Sinon, afficher le formulaire de connexion
+        document.getElementById('login-form').style.display = 'block';
+        document.getElementById('login-success').style.display = 'none';
+    }
+
+    // Ajouter un gestionnaire d'événements pour le bouton de connexion
+    document.getElementById('login-button').addEventListener('click', function() {
+        var nom = document.getElementById('nom').value;
+        var motDePasse = document.getElementById('mot-de-passe').value;
+
+        // Effectuer la requête de connexion
+        fetch('http://192.168.65.243:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nom: nom,
+                motDePasse: motDePasse
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Utilisateur ou mot de passe incorrect.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Stocker le token dans un cookie de session
+            document.cookie = `token=${data.token}; path=/;`;
+            // Rediriger vers une autre page après la connexion réussie
+            window.location.href = 'pagemeteo.html'; // Remplacez 'index.html' par l'URL de la page vers laquelle vous souhaitez rediriger
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('error-message').innerText = error.message;
+        });
+    });
+};
+
+// Fonction pour récupérer la valeur d'un cookie
+function getCookie(name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return match[2];
+}
