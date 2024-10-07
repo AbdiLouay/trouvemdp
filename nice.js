0f8fbaf311a9e562dcf630f1837a9559f5405063
AbdiLouay <ablouay5@gmail.com>
2024-10-07 16:06:18 +0200

    8

--- nice.js
+++ nice.js
@@ -1,3 +1,20 @@
+// Fonction pour vérifier si le token existe dans le localStorage ou les cookies
+function checkToken() {
+    // Vérifier si le token est présent dans le localStorage
+    const tokenLocalStorage = localStorage.getItem('token');
+    
+    // Vérifier si le token est présent dans les cookies
+    const tokenCookie = document.cookie.split('; ').find(row => row.startsWith('token='));
+    
+    // Si le token n'est pas dans le localStorage ou dans les cookies, rediriger vers la page de connexion
+    if (!tokenLocalStorage && !tokenCookie) {
+        window.location.href = 'index.html';
+    }
+}
+
+// Appeler la fonction pour vérifier le token dès le chargement de la page
+checkToken();
+
 // Récupérer le bouton de déconnexion
 const logoutButton = document.getElementById('logout-button45');
 
@@ -6,9 +23,9 @@ logoutButton.addEventListener('click', function() {
     // Supprimer le token du localStorage
     localStorage.removeItem('token');
     
-    // Supprimer le token du cookie
+    // Supprimer le token des cookies
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-
+    
     // Rediriger vers la page de connexion
     window.location.href = 'index.html';
 });
